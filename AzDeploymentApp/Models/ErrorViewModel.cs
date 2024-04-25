06e47849b17e15eb7946d171835a472d0283d956
Vranda Daga <amit.mishra@zensar.com>
2024-04-25 12:22:30 +0530

    Add project files.

---
+++ AzDeploymentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzDeploymentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
